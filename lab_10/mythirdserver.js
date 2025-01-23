62549d5c03e741b53812f58e949d382b0bf20ef6
Zuhaib Adnaan Syed <sz135760@gmail.com>
2025-01-23 12:15:20 +0000

    ..

--- lab_10/mythirdserver.js
+++ lab_10/mythirdserver.js
@@ -1,10 +1,10 @@
 var http = require('http');
 var knockknock = require('knock-knock-jokes');
-var currentdate = require('./mymodule');
 http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write("The date and time are currently: " + currentdate.myDateTime());
- res.end('Hello World!');
- var randomJoke = knockknock()
- res.end(randomJoke);
+    var randomJoke = knockknock()
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(randomJoke);
 }).listen(8080);
+
+
+
